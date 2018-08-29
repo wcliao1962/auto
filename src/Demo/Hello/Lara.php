c0e1f5ccb2c3b8027deb030510da17a86928ec9b
廖文忠 <wcliao1962@gmail.com>
2018-08-29 20:32:41 +0800

    設計Demo下之子命名空間Hello的Lara及Someone類別

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,10 @@
+<?php
+namespace Demo\Hello;
+
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+}
---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,10 @@
+<?php
+namespace Demo\Hello;
+
+class Someone
+{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.PHP_EOL;
+    }
+}
